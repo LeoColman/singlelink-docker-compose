e64d8fde6d0d2289bd2fbabdd5b3e0e3e0184351
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-06-15 12:47:44 -0300

    Add sample docker-compose; Tweak entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
-if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
-    touch $CONTAINER_ALREADY_STARTED
-    npm run build
-    npm run no_dotenv_db:migrate
-fi
+npm run build
+npm run no_dotenv_db:migrate
 npm run start
